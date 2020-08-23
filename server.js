ca3da3816931f1a374efc9bc21e75db1006349ec
Andrii Vasylchenko <andrii@ibm.com>
2020-08-23 14:25:44 -0400

    restructuring

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 var express = require('express');
 var monk = require('monk');
-var db = monk('mongodb/nodetest1');
 var basicAuth = require('basic-auth-connect');
 var app = express();
 
 app.use(express.static('/mnt/storage/www'));
 
+const db = monk('mongodb/nodetest1');
+const collection = db.get('logscollection');
+
 app.get('/database/getData', basicAuth(process.env.API_USER, process.env.API_PASSWORD), function (req, res) {
-  // var db = req.db;
-  var collection = db.get('logscollection');
-  collection.find({},{},function(error, docs){
-    if (!error) {
-      res.json(docs);
-    } else {
-      res.status(500).json(error);
-    }  
-  })
+  collection.find().then((docs) => {
+    res.json(docs);
+  }).catch((error) => {
+    res.status(500).json(error);
+  }).then(() => db.close())
 })
   
 app.post('/database/postData', basicAuth(process.env.API_USER, process.env.API_PASSWORD), function (req, res) {
-  // var db = req.db;
-  var collection = db.get('logscollection');
-  collection.insert(JSON.parse(req.body), function (error, doc) {
-    if (!error) {
-      res.json(doc);
-    } else {
-      res.status(500).json(error);
-    }
-  })
+  collection.insert(req.body).then((docs) => {
+    res.json(docs);
+  }).catch((error) => {
+    res.status(500).json(error);
+  }).then(() => db.close())
 })
 
+app.get('/database/removeData', basicAuth(process.env.API_USER, process.env.API_PASSWORD), function (req, res) {
+  collection.remove({}).then((docs) => {
+    res.json(docs);
+  }).catch((error) => {
+    res.status(500).json(error);
+  }).then(() => db.close())
+})
 
 app.listen(8080, '0.0.0.0', function() {
   console.log("server started");
