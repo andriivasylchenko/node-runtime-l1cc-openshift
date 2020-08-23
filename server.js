a16239303d44350f481b7784c39c2defcdbe477e
Andrii Vasylchenko <andrii@ibm.com>
2020-08-23 14:47:01 -0400

    adding body parser

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var monk = require('monk');
 var basicAuth = require('basic-auth-connect');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('/mnt/storage/www'));
+app.use(bodyParser.json());
 
 const db = monk('mongodb/nodetest1');
 const collection = db.get('logscollection');
@@ -17,7 +19,7 @@ app.get('/database/getData', basicAuth(process.env.API_USER, process.env.API_PAS
 })
   
 app.post('/database/postData', basicAuth(process.env.API_USER, process.env.API_PASSWORD), function (req, res) {
-  collection.insert([{ woot: 'bar' }, { woot: 'baz' }]).then((docs) => {
+  collection.insert(req.body).then((docs) => {
     res.json(docs);
   }).catch((error) => {
     res.status(500).json(error);
